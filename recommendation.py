290962b0c4319d15ac5bb0775d731c2c342065fc
Naila51 <nailarazzaq070@gmail.com>
2026-08-19 18:51:53 +0500

    Update imports and return top career recommendations
    Added imports for Streamlit and NumPy. Fixed import statements for scikit-learn.

--- recommendation.py
+++ recommendation.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+from scikitlearn.feature_extraction.text import TfidfVectorizer
+from scikitlearn.metrics.pairwise import cosine_similarity
+streamlit
+pandas
+numpy
+scikit-learn
 
 
 def load_dataset():
